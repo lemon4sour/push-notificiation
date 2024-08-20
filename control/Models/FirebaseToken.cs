049b2826937ac43671efce3be1c313e59f0ae123
lemon4sour <96292208+lemon4sour@users.noreply.github.com>
2024-08-20 18:15:00 +0300

    Revert "system"

--- control/Models/FirebaseToken.cs
+++
@@ -1,8 +0,0 @@
-namespace control.Models;
-
-public class FirebaseToken
-{
-    public long timestamp { get; set; }
-    public string userAgent { get; set; }
-    public List<string> topics { get; set;}
-}
